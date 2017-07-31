ef5493cf4682fcdf95f0475c0ad0bcc9752d8664
buoyantair <buoyantair@gmail.com>
2017-07-31 17:15:52 +0530

    Completed - Constant Definitions & Function Definitions

---
+++ How to Code - Simple Data/1a Beginning Student Language/function-definitions-starter.rkt
@@ -0,0 +1,20 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname function-definitions-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+;; function-definitions-starter.rkt
+
+;(define (bulb c)
+;  (circle 40 "solid" c))
+;
+;(above (bulb "red")         
+;       (bulb "yellow")
+;       (bulb "green"))
+;
+;
+;(define (pythag a b)
+;  (sqrt (+ (sqr a) (sqr b))))
+;
+;(pythag 2 2)
+
